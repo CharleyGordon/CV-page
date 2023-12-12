2e3a834c574b02bf45b7799bcaa0bc69837e9ffb
CharleyGordon <101750733+CharleyGordon@users.noreply.github.com>
2023-12-12 18:06:37 +0100

    Add files via upload

---
+++ scripts/main.js
@@ -0,0 +1,70 @@
+const header = document.querySelector("header");
+
+const headerAnchors = Array.from(header.querySelectorAll("a"));
+
+function type({ element, pendingClassname, charArray = [], finished = false }) {
+  if (!element.classList.contains(pendingClassname))
+    element.classList.add(pendingClassname);
+  if (charArray.length === 0 && !finished) {
+    const { textContent } = element;
+    element.textContent = "";
+    charArray = textContent.split("");
+  }
+  if (charArray.length > 0) {
+    if (charArray.length === 1) finished = true;
+    setTimeout(() => {
+      const string = charArray.shift();
+      element.textContent += string;
+      return type({ element, pendingClassname, charArray, finished });
+    }, 30);
+  } else {
+    element.classList.remove(pendingClassname);
+  }
+}
+
+function lightSection(event, className = "current") {
+  let { target } = event;
+  if (!target.matches("a")) return;
+  for (anchor of headerAnchors) {
+    anchor.classList.remove(className);
+  }
+  target.classList.add(className);
+}
+
+function preventDeatailsClose(event) {
+  const { target } = event;
+  if (target.closest("details")) event.preventDefault();
+}
+
+function redirect(event) {
+  const { target } = event;
+  const figure = target.closest("figure");
+  if (!figure) return;
+  const anchor = figure.querySelector("a");
+  if (target.matches("a") || !anchor) return;
+  anchor.click();
+}
+
+header.addEventListener("click", lightSection);
+
+document.body.addEventListener("click", preventDeatailsClose);
+
+document.body.addEventListener("click", redirect);
+
+const effects = {
+  elements: {
+    figcaptionCodes: document.querySelectorAll("figcaption p"),
+  },
+  classnames: {
+    pending: "pending",
+  },
+  functions: {
+    typewriter() {
+      effects.elements.figcaptionCodes.forEach((code) => {
+        type({ element: code, pendingClassname: effects.classnames.pending });
+      });
+    },
+  },
+};
+
+effects.functions.typewriter();
